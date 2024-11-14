3254332bd6f5876c64a976309e770fbf2541a1b9
natan <natansdj@users.noreply.github.com>
2024-11-15 01:32:02 +0700

    update

---
+++ recover_db_jellyfin.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd /var/lib/jellyfin/data
+
+### Recover library.db
+sqlite3 library.db ".recover" | sqlite3 library-recovered.db
+
+#2. We will now check the integrity of our recovered database (as above) using
+sqlite3 library-recovered.db "PRAGMA integrity_check"
+
+#3. Make a copy of both library.db and library-recovered.db
+cp library.db library.db.bak
+cp library-recovered.db library-recovered.db.bak
+
+#4. Rename library.db to library.old
+mv library.db library.old
+
+#5. Rename library-recovered.db to library.db
+mv library-recovered.db library.db
+
+#6. Restart Jellyfin Server
+docker restart jellyfin
+
+
